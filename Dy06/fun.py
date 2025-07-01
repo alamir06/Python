6b27a9cf31b64bef9a8e988b456d5de53c4e4b4e
Alamirew <akliluarse@gmail.com>
2025-07-01 23:46:00 +0300

    reeborgs funct commit

---
+++ Dy06/fun.py
@@ -0,0 +1,18 @@
+# function in python: is a block of code or subprogram that perform specific task
+# it function definitionand calling center
+# it has built and user defined part
+# e.g. built in :print(),len(),..
+# for user defined we must use def key word and function name which is we want and meaningfull plus identifier structure and followed by parenthesis
+def my_function(): # function definition
+    print("hello function in python")
+my_function() #function call
+
+def print_even():
+    print(4)
+
+for i in range(101):
+        print_even()
+
+    
+    
+    
